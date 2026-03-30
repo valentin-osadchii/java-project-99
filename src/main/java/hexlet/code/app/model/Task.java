35158780f921e170578bd6c2990f613c25ddf602
VOsadchii <909404@gmail.com>
2026-03-30 23:23:07 +0300

    Fix missing annotation

--- src/main/java/hexlet/code/app/model/Task.java
+++ src/main/java/hexlet/code/app/model/Task.java
@@ -1,6 +1,7 @@
 package hexlet.code.app.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,6 +16,7 @@ import java.time.LocalDate;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+@Entity
 @Data
 public class Task implements BaseEntity {
 
