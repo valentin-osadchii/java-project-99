0d51a1d06f479e122058dc74bb62c2a73063420b
VOsadchii <909404@gmail.com>
2026-04-08 23:44:30 +0300

    sonar fixes 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,12 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
 	java
-	id("org.springframework.boot") version libs.versions.spring.boot.get()
+	alias(libs.plugins.spring.boot)
 	alias(libs.plugins.spring.dependency.management)
 	id("checkstyle")
-	id("application")
-	id("org.sonarqube") version libs.versions.sonar.plugin.get()
+	application
+	alias(libs.plugins.sonarqube)
 	jacoco
-
 }
 
 group = "hexlet.code"
@@ -32,15 +31,15 @@ repositories {
 
 dependencies {
 	// Spring Boot starters
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation(libs.spring.boot.starter)
+	implementation(libs.spring.boot.starter.web)
+	implementation(libs.spring.boot.starter.data.jpa)
+	implementation(libs.spring.boot.starter.validation)
+	implementation(libs.spring.boot.starter.security)
+	implementation(libs.spring.boot.starter.oauth2.resource.server)
 
 	// OpenAPI / Swagger
-	implementation(libs.springdoc)
+	implementation(libs.springdoc.openapi)
 	implementation(libs.jackson.databind.nullable)
 
 	// Object mapping
@@ -52,21 +51,21 @@ dependencies {
 	annotationProcessor(libs.lombok)
 	annotationProcessor(libs.lombok.mapstruct.binding)
 
-	// Databases
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("org.postgresql:postgresql")
+	// Database
+	runtimeOnly(libs.h2)
+	runtimeOnly(libs.postgresql)
 
 	// Configuration metadata
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor(libs.spring.boot.configuration.processor)
 
 	// Testing
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+	testImplementation(libs.spring.boot.starter.test) {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("org.springframework:spring-webmvc")
-	testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
-	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly(libs.junit.platform.launcher)
+	testImplementation(libs.spring.webmvc)
+	testImplementation(libs.spring.boot.test.autoconfigure)
+	testImplementation(libs.spring.security.test)
 	testImplementation(libs.json.unit.assertj)
 	testImplementation(libs.instancio)
 	testImplementation(libs.datafaker)
