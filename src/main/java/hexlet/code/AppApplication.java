0547487fab25701e40bdd382702e7bc9ad475e83
VOsadchii <909404@gmail.com>
2026-04-08 10:25:04 +0300

    Add @SpringBootApplication and @EnableJpaAuditing to AppApplication wrapper class

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,7 +1,11 @@
 package hexlet.code;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@SpringBootApplication
 public class AppApplication extends hexlet.code.app.AppApplication {
 
     public static void main(String[] args) {
